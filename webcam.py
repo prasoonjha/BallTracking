ca4eb12de55ed49625c810e632f23000d50de2fe
Prasoon Jha <prasoonjha69@gmail.com>
2020-03-15 08:20:01 +0530

    first commit

---
+++ webcam.py
@@ -0,0 +1,9 @@
+Webcam webcam = Webcam.getDefault();
+webcam.open();
+try {
+  ImageIO.write(webcam.getImage(), "PNG", new File("test.png"));
+} catch (IOException e) {
+  e.printStackTRace();
+} finally {
+  webcam.close();
+}
